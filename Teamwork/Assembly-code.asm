2aa71b4122c8a4cce14c9594c0fbab40e0239726
miermontoto <underscoreBis@gmail.com>
2021-03-13 18:23:52 +0100

    ControlWithInlineAssembly()
    también es mejor ControlinAsmblyFile() y FailedTest()

--- Teamwork/Assembly-code.asm
+++ Teamwork/Assembly-code.asm
@@ -3,19 +3,19 @@
 
 .code
 
-; Complete the procedure
 IsValidAssembly PROC
 ; ebp es equivalente a r6
 ; esp es equivalente a r7
+; Primero se completa el "Prólogo"
 
 push ebp
 mov ebp, esp
 
 push ebx
 push ecx
-; no se guarda eax porque es lo que devuelve el proc.
+; no se guarda eax porque es lo que devuelve el procedimiento
 
-mov eax, [ebp + 8] ; num1 -> eax
+mov eax, [ebp + 8 ] ; num1 -> eax
 mov ebx, [ebp + 12] ; num2 -> ebx
 mov ecx, [ebp + 16] ; num3 -> ecx
 
@@ -23,40 +23,27 @@ and eax, 000000FFh ; Máscara de los 8 bits más bajos de num1
 and ebx, 00000080h ; Máscara del 8vo bit de num2
 and ecx, 00000080h ; Máscara del 8vo bit de num3
 
-; Se compara num2 con num3
+; Se comprueba: eax > 238
+cmp eax, 238
+ja firstCheck
+jmp wrong ; Si no es verdad, no es necesario comprobar más, ya que no puede
+          ; darse que ambas sean correctas.
+firstCheck:
+
+; Se comprueba: ebx == ecx
 ; Como ambos solo tienen el octavo bit, se pueden comparar
 ; sin necesidad de mover los bits.
 cmp ebx, ecx
-je bcTrue
-mov edx, 0
-jmp nextOne
-bcTrue:
-mov edx, 1
-nextOne:
-; El estado de la comprobación se guarda en edx
+je secondCheck
+jmp wrong
 
-; Se comprueba que num1 > 238
-cmp eax, 238
-ja bmTrue
-mov ebx, 0
-jmp nextTwo
-bmTrue:
-mov ebx, 1
-nextTwo:
-; El estado de la comprobación se guarda en ebx
-
-; Se comprueban que se hayan cumplido ambas condiciones
-; Esto ocurre si ebx y edx son ambas 1.
-mov eax, 0
-cmp ebx, 1
-je initCheck
-jmp final
-initCheck:
-cmp ebx, edx
-je finalCheck
-jmp final
-finalCheck:
+secondCheck:
 mov eax, 1
+jmp final
+
+wrong:
+mov eax, 0
+
 final:
 
 pop ecx
--- Teamwork/Teamwork.cpp
+++ Teamwork/Teamwork.cpp
@@ -5,38 +5,63 @@ extern "C" bool IsValidAssembly(int a, int b, int c);
 #include <iostream>
 using namespace std;
 
-void FailedTest() {
-	cout << "Se han introducido valores incorrectos." << endl;
+void FailedTest(char s[]) {
+	cout << s << endl;
 	exit(0);
 }
 
 void ControlInAsmblyFile() {
+	cout << "ControlInAsmblyFile() " << endl;
 	int num1, num2, num3;
+	cout << "num1: ";
 	cin >> num1;
+	cout << "num2: ";
 	cin >> num2;
+	cout << "num3: ";
 	cin >> num3;
 
-	if (!IsValidAssembly(num1, num2, num3)) {
-		cout << 0 << endl;
-		FailedTest();
-	}
+	if (!IsValidAssembly(num1, num2, num3)) FailedTest("Eso no es correcto");
 }
 
-void ControlWithInlineAssembly() {
+void ControlWithInlineAssembly(){
 	int check;
+	int status = 0;
+	cout << "ControlWithInlineAssembly() ";
 	cin >> check;
+	cout << endl;
 	__asm {
+		mov eax, check
+		mov ebx, check
+		and eax, 0xFFFF0000
+		and ebx, 0x0000FFFF
+		shr eax, 16
+		cmp eax, ebx
+		je true
+		jmp final
 
+		true:
+		inc status
+		final:
 	}
+
+	if (!((bool)status)) FailedTest("Eso no es correcto");
+}
+
+void ControlString() {
+
+}
+
+void BitControl() {
+
 }
 
 int main() {
-	//ControlString();
-	//BitControl();
+	ControlString();
+	BitControl();
 	ControlInAsmblyFile();
 	ControlWithInlineAssembly();
 
-	cout << "Correcto." << endl;
+	cout << "Correcto" << endl;
 	// Si se llega a esta línea es que ningún método ha
 	// hecho exit(), con lo que se han completado todas
 	// las etapas satisfactoriamente.
